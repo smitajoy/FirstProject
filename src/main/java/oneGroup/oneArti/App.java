d6f9448dea133687f2ac3b568676d8e5dec965c7
smitajoy <30336216+smitajoy@users.noreply.github.com>
2017-07-21 12:23:12 +0530

    Add files via upload

---
+++ src/main/java/oneGroup/oneArti/App.java
@@ -0,0 +1,17 @@
+package oneGroup.oneArti;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World! Dup1" );
+    }
+    
+    public int square(int x) {
+    	return x*x;
+    }
+}
---
+++ src/test/java/oneGroup/oneArti/AppTest.java
@@ -0,0 +1,41 @@
+package oneGroup.oneArti;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+    extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public AppTest( String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( AppTest.class );
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp()
+    {
+//        assertTrue( true );
+		App test = new App();
+		int output = test.square(5);
+		assertEquals(25, output);
+    }
+}
